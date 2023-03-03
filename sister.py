6cafa04458a92e4881c5b4d7cc4845191e0a2e8f
SabareesanArjunasamy <sabareesan833@gmail.com>
2023-03-04 02:48:07 +0530

    feat(bot) responds to hello

---
+++ sister.py
@@ -0,0 +1,25 @@
+import os
+import discord
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv('TOKEN')
+
+client = discord.Client(intents=discord.Intents.default())
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+    
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == 'hello':
+        await message.channel.send("Hi There...!")
+        
+        
+client.run(token)
